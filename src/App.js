9652b61c02ea1fcaac1e216cb01d1e775165b10c
joshua-miles <joshua@christetulit.org>
2019-04-26 22:51:00 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router'
+import MobyDick from './MobyDick';
+import OldManAndTheSea from './OldManAndTheSea';
+
+// 1) Render a Route for MobyDick: moby-dick
+// 2) Render a Route for OldManAndTheSea: old-man-and-the-sea
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter >
+        { /* ? */ }
+      </BrowserRouter> 
     </div>
   );
 }
