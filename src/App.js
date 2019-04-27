732cda25f82d6c83f6cbca3073d6f28a77a0acc0
joshua-miles <joshua@christetulit.org>
2019-04-26 22:56:55 -0500

    -

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Route } from 'react-router'
 import MobyDick from './MobyDick';
 import OldManAndTheSea from './OldManAndTheSea';
+import Home from './Home';
 
 // 1) Render a Route for MobyDick: moby-dick
 // 2) Render a Route for OldManAndTheSea: old-man-and-the-sea
@@ -11,7 +12,9 @@ function App() {
   return (
     <div>
       <BrowserRouter >
-        { /* ? */ }
+        <Route path='/old-man-and-the-sea' component={OldManAndTheSea} />
+        <Route path='/moby-dick' component={MobyDick} />
+        <Route path='/' component={Home} />
       </BrowserRouter> 
     </div>
   );
